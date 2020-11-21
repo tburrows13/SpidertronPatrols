0fd5336d84cb76879da03fad163e862f26fd1758
Tom Burrows <tomburrows13@icloud.com>
2020-11-21 01:10:40 +0000

    data.lua reorganisation

--- data.lua
+++ data.lua
@@ -1,11 +1,11 @@
 require 'shortcut'
 
+
 local waypoint_remote = table.deepcopy(data.raw["spidertron-remote"]["spidertron-remote"])
 waypoint_remote.name = "spidertron-remote-waypoint"
 waypoint_remote.flags = {"hidden"}
 waypoint_remote.icon = "__SpidertronWaypoints__/graphics/waypoint-remote.png"
 waypoint_remote.icon_mipmaps = 1
-
 
 local patrol_remote = table.deepcopy(data.raw["spidertron-remote"]["spidertron-remote"])
 patrol_remote.name = "spidertron-remote-patrol"
@@ -13,6 +13,9 @@ patrol_remote.flags = {"hidden"}
 patrol_remote.icon = "__SpidertronWaypoints__/graphics/patrol-remote.png"
 patrol_remote.icon_mipmaps = 1
 
+data:extend{waypoint_remote, patrol_remote}
+
+-- Mode selection
 local direct_control = {
   type = "custom-input",
   name = "waypoints-go-to-direct-mode",
@@ -31,54 +34,83 @@ local patrol_control = {
   key_sequence = "SHIFT + X",
   order = "dc"
 }
+data:extend{direct_control, waypoint_control, patrol_control}
 
-data:extend({
-  {
-    type = "custom-input",
-    name = "clear-spidertron-waypoints",
-    key_sequence = "SHIFT + mouse-button-1",
-    consuming = "none",
-    order = "aa"
-  },
-  {
-    type = "custom-input",
-    name = "waypoints-disconnect-remote",
-    key_sequence = "CONTROL + mouse-button-1",
-    consuming = "none",
-    order = "ab"
-  },
-  {
-    type = "custom-input",
-    name = "waypoints-complete-patrol",
-    key_sequence = "ALT + mouse-button-1",
-    consuming = "none",
-    order = "ac"
-  },
-  {
-    type = "custom-input",
-    name = "waypoints-change-wait-conditions",
-    key_sequence = "Y",
-    consuming = "none",
-    order = "ad"
-  },
-  {
-    type = "custom-input",
-    name = "waypoints-change-default-wait-conditions",
-    key_sequence = "SHIFT + Y",
-    consuming = "none",
-    order = "ae"
-  },
-  waypoint_remote,
-  patrol_remote,
-  direct_control,
-  waypoint_control,
-  patrol_control,
-  {
-    type = "custom-input",
-    name = "waypoints-gui-confirm",
-    key_sequence = "RETURN",
-    order = "ae"
-  },
+
+-- Mode scrolling
+local scroll_forwards = {
+  type = "custom-input",
+  name = "waypoints-mode-scroll-forwards",
+  key_sequence = "",
+  linked_game_control = "cycle-blueprint-forwards"
+}
+local scroll_backwards = {
+  type = "custom-input",
+  name = "waypoints-mode-scroll-backwards",
+  key_sequence = "",
+  linked_game_control = "cycle-blueprint-backwards"
+}
+local scroll_forwards_key = {
+  type = "custom-input",
+  name = "waypoints-mode-scroll-forwards-key",
+  key_sequence = "KP_MINUS",
+  order="ba"
+}
+local scroll_backwards_key = {
+  type = "custom-input",
+  name = "waypoints-mode-scroll-backwards-key",
+  key_sequence = "KP_PLUS",
+  order="bb"
+}
+data:extend{scroll_forwards, scroll_backwards, scroll_forwards_key, scroll_backwards_key}
+
+
+-- Misc inputs
+local clear_waypoints_key = {
+  type = "custom-input",
+  name = "clear-spidertron-waypoints",
+  key_sequence = "SHIFT + mouse-button-1",
+  consuming = "none",
+  order = "aa"
+}
+local disconnect_remote_key = {
+  type = "custom-input",
+  name = "waypoints-disconnect-remote",
+  key_sequence = "CONTROL + mouse-button-1",
+  consuming = "none",
+  order = "ab"
+}
+local complete_patrol_key = {
+  type = "custom-input",
+  name = "waypoints-complete-patrol",
+  key_sequence = "ALT + mouse-button-1",
+  consuming = "none",
+  order = "ac"
+}
+local change_wait_time_key = {
+  type = "custom-input",
+  name = "waypoints-change-wait-conditions",
+  key_sequence = "Y",
+  consuming = "none",
+  order = "ad"
+}
+local change_default_wait_time_key = {
+  type = "custom-input",
+  name = "waypoints-change-default-wait-conditions",
+  key_sequence = "SHIFT + Y",
+  consuming = "none",
+  order = "ae"
+}
+local confirm_gui_key = {
+  type = "custom-input",
+  name = "waypoints-gui-confirm",
+  key_sequence = "RETURN",
+  order = "ae"
+}
+data:extend{clear_waypoints_key, disconnect_remote_key, complete_patrol_key, change_wait_time_key, change_default_wait_time_key, confirm_gui_key}
+
+-- Allows getting movement control events to detect when to cancel a spidertron's waypoints
+data:extend{
   {
     type = "custom-input",
     name = "move-right-custom",
@@ -102,9 +134,8 @@ data:extend({
     name = "move-down-custom",
     key_sequence = "",
     linked_game_control = "move-down"
-  },
-
-})
+  }
+}
 
 local styles = data.raw["gui-style"]["default"]
 styles.waypoints_switch_padding = {type = "switch_style", parent="switch", top_padding=3}  -- Fixes height of switch to be the same as its labels
--- shortcut.lua
+++ shortcut.lua
@@ -78,55 +78,5 @@ local patrol_toggle = {
   order = "cb"
 }
 
-local scroll_forwards = {
-  type = "custom-input",
-  name = "waypoints-mode-scroll-forwards",
-  key_sequence = "",
-  linked_game_control = "cycle-blueprint-forwards"
-}
-local scroll_backwards = {
-  type = "custom-input",
-  name = "waypoints-mode-scroll-backwards",
-  key_sequence = "",
-  linked_game_control = "cycle-blueprint-backwards"
-}
-local scroll_forwards_key = {
-  type = "custom-input",
-  name = "waypoints-mode-scroll-forwards-key",
-  key_sequence = "KP_MINUS",
-  order="ba"
-}
-local scroll_backwards_key = {
-  type = "custom-input",
-  name = "waypoints-mode-scroll-backwards-key",
-  key_sequence = "KP_PLUS",
-  order="bb"
-}
 
---[[local waypoint_toggle_click = {
-  type = "custom-input",
-  name = "waypoints-waypoint-mode-click",
-  key_sequence = "mouse-button-2",
-}]]
---[[local patrol_toggle_click = {
-  type = "custom-input",
-  name = "waypoints-patrol-mode-click",
-  key_sequence = "SHIFT + mouse-button-2",
-}]]
-
-
-
-
---[[local pick_item = {
-  type = "custom-input",
-  name = "pick-item-custom",
-  key_sequence = "",
-  linked_game_control = "pick-item"
-}]]
-
-
---local p
-
-
-data:extend({waypoint_shortcut, patrol_shortcut, waypoint_toggle, patrol_toggle, scroll_forwards, scroll_backwards, scroll_forwards_key, scroll_backwards_key})
---data:extend({pick_item})
+data:extend({waypoint_shortcut, patrol_shortcut, waypoint_toggle, patrol_toggle})
