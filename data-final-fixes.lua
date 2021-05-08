4045a3477e7ef14c49233d4d0b7a7e930a77021c
Tom Burrows <tomburrows13@icloud.com>
2021-05-08 14:28:27 +0100

    Reduce maximal_height when AutoTrash is active

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,3 +3,11 @@ if spiderling_grid then
   -- Won't exist if spiderlings are disabled or if another mod removes it
   spiderling_grid.equipment_categories = data.raw["equipment-grid"]["spidertron-equipment-grid"].equipment_categories
 end
+
+if mods["AutoTrash"] then
+  -- Further workarounds for https://forums.factorio.com/viewtopic.php?f=7&t=98151
+  local styles = data.raw["gui-style"].default
+
+  styles.at_right_scroll_pane.maximal_height = 150
+  styles.sp_relative_stretchable_frame.maximal_height = 670
+end
