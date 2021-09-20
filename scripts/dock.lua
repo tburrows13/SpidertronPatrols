4bb733a6728b74ac715a4c6eea72cb895def4b54
Tom Burrows <tomburrows13@icloud.com>
2021-09-21 00:59:56 +0100

    Add on_entity_cloned support to docks/spidertrons

--- scripts/dock.lua
+++ scripts/dock.lua
@@ -6,10 +6,12 @@ local function on_built(event)
     if entity.name == "sp-spidertron-dock-0" then
       global.spidertron_docks[entity.unit_number] = {dock = entity}
       script.register_on_entity_destroyed(entity)
-    else
-      if entity.type == "spider-vehicle" then
+    elseif entity.type == "spider-vehicle" then
         script.register_on_entity_destroyed(entity)
-      end
+    else
+      -- from on_entity_cloned, a non-zero-capacity dock has been created
+      entity = replace_dock(entity, "sp-spidertron-dock-0")
+      global.spidertron_docks[entity.unit_number] = {dock = entity}
     end
   end
 end
@@ -17,6 +19,14 @@ script.on_event(defines.events.on_built_entity, on_built, {{filter = "name", nam
 script.on_event(defines.events.on_robot_built_entity, on_built, {{filter = "name", name = "sp-spidertron-dock-0"}, {filter = "type", type = "spider-vehicle"}})
 script.on_event(defines.events.script_raised_revive, on_built, {{filter = "name", name = "sp-spidertron-dock-0"}, {filter = "type", type = "spider-vehicle"}})
 script.on_event(defines.events.script_raised_built, on_built, {{filter = "name", name = "sp-spidertron-dock-0"}, {filter = "type", type = "spider-vehicle"}})
+
+local function on_entity_cloned(event)
+  local entity = event.destination
+  if entity.type == "spider-vehicle" or string.sub(entity.name, 0, 19) == "sp-spidertron-dock-" then
+    on_built{entity = entity}
+  end
+end
+script.on_event(defines.events.on_entity_cloned, on_entity_cloned, {{filter = "type", type = "container"}, {filter = "type", type = "logistic-container"}, {filter = "type", type = "spider-vehicle"}})
 
 function on_entity_destroyed(event)
   local unit_number = event.unit_number
