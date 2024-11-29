89ffa68167fe7963d5e191ea46bb506331356af4
Tom Burrows <tomburrows13@icloud.com>
2024-11-29 04:31:45 +0000

    Fix amonut typo in unused IR3 code

--- prototypes/industrial-revolution.lua
+++ prototypes/industrial-revolution.lua
@@ -10,12 +10,12 @@ if spiderling_enabled then
   util.add_prerequisite("sp-spiderling", "military-2")
 
   data.raw.recipe["sp-spiderling"].ingredients = {
-    {type="item", name="computer-mk2", amonunt=4},
-    {type="item", name="gyroscope", amonunt=1},  -- Ideally would be steel-frame-turret, but this is only unlocked after chrome
-    {type="item", name="copper-coil", amonunt=4},
-    {type="item", name="low-density-structure", amonunt=60},
-    {type="item", name="steel-piston", amonunt=20},
-    {type="item", name="rocket-launcher", amonunt=1},
+    {type="item", name="computer-mk2", amount=4},
+    {type="item", name="gyroscope", amount=1},  -- Ideally would be steel-frame-turret, but this is only unlocked after chrome
+    {type="item", name="copper-coil", amount=4},
+    {type="item", name="low-density-structure", amount=60},
+    {type="item", name="steel-piston", amount=20},
+    {type="item", name="rocket-launcher", amount=1},
   }
 end
 
@@ -24,7 +24,7 @@ if dock_enabled then
   util.add_prerequisite("sp-spidertron-automation", "fast-inserter")
 
   data.raw.recipe["sp-spidertron-dock"].ingredients = {
-    {type="item", name="steel-chest", amonunt=4},
-    {type="item", name="fast-inserter", amonunt=8},
+    {type="item", name="steel-chest", amount=4},
+    {type="item", name="fast-inserter", amount=8},
   }
 end
