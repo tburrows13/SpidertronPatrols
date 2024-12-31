634d1380a5be9380d96d267e71b3296bf1e37226
Tom Burrows <tomburrows13@icloud.com>
2024-12-31 17:09:27 +0000

    Add patrol remote shortcut

--- prototypes/custom-input.lua
+++ prototypes/custom-input.lua
@@ -1,6 +1,31 @@
 -- Misc inputs
 data:extend{
   {
+    type = "shortcut",
+    name = "sp-give-spidertron-patrol-remote",
+    order = "e-b[spidertron-remote]",
+    action = "spawn-item",
+    localised_name = {"shortcut-name.sp-give-spidertron-patrol-remote", SPIDERTRON_NAME},
+    associated_control_input = "sp-give-spidertron-patrol-remote",
+    technology_to_unlock = "spidertron",  -- Overwritten at bottom of file if spiderling is enabled
+    unavailable_until_unlocked = true,
+    item_to_spawn = "sp-spidertron-patrol-remote",
+    icon = "__SpidertronPatrols__/graphics/shortcut/spidertron-patrol-remote-shortcut-x56.png",
+    icon_size = 56,
+    small_icon = "__SpidertronPatrols__/graphics/shortcut/spidertron-patrol-remote-shortcut-x56.png",
+    small_icon_size = 56,
+  },
+  {
+    type = "custom-input",
+    name = "sp-give-spidertron-patrol-remote",
+    key_sequence = "CONTROL + ALT + A",
+    --controller_key_sequence = "controller-lefttrigger + controller-y",
+    block_modifiers = true,
+    consuming = "game-only",
+    item_to_spawn = "sp-spidertron-patrol-remote",
+    action = "spawn-item"
+  },
+  {
     type = "custom-input",
     name = "sp-use-item-custom",
     key_sequence = "",
@@ -68,4 +93,5 @@ data:extend{
 
 if settings.startup["sp-enable-spiderling"].value then
   data.raw["shortcut"]["give-spidertron-remote"].technology_to_unlock = "sp-spiderling"
+  data.raw["shortcut"]["sp-give-spidertron-patrol-remote"].technology_to_unlock = "sp-spiderling"
 end
